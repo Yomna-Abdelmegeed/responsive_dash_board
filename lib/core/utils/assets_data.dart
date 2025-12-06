9f48b52239ec6a96a78630eef688eaf99cf2f256
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-12-07 00:04:24 +0200

    feat: add all image assets and define their paths in Assets class.

---
+++ lib/core/utils/assets_data.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String imagesBalance = "assets/images/Balance.png";
+  static const String imagesCardBg = "assets/images/card_bg.png";
+  static const String imagesDashboard = "assets/images/Dashboard.png";
+  static const String imagesExpenses = "assets/images/expenses.png";
+  static const String imagesFace1 = "assets/images/face_1.png";
+  static const String imagesFace2 = "assets/images/face_2.png";
+  static const String imagesFace3 = "assets/images/face_3.png";
+  static const String imagesFace4 = "assets/images/face_4.png";
+  static const String imagesGallery = "assets/images/gallery.png";
+  static const String imagesIncome = "assets/images/Income.png";
+  static const String imagesInvestments = "assets/images/Investments.png";
+  static const String imagesLogout = "assets/images/logout.png";
+  static const String imagesSetting = "assets/images/setting.png";
+  static const String imagesStatistics = "assets/images/Statistics.png";
+  static const String imagesTransaction = "assets/images/Transaction.png";
+  static const String imagesWallet = "assets/images/Wallet.png";
+}

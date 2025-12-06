61825a276f3b79af66a953a3f135cdc6ff200757
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-12-07 00:27:47 +0200

    feat: add AppColors class with all app color definitions.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF064061);
+
+  static const Color secondary = Color(0xFF4EB7F2);
+
+  static const Color blue = Color(0xFF208CC8);
+
+  static const Color green = Color(0xFF7DD97B);
+  static const Color red = Color(0xFFF3735E);
+
+  static const Color grey = Color(0xFFAAAAAA);
+  static const Color lightGrey = Color(0xFFFAFAFA);
+  static const Color beige = Color(0xFFE2DECD);
+
+  static const Color background = Color(0xFFE5E5E5);
+  static const Color white = Colors.white;
+}

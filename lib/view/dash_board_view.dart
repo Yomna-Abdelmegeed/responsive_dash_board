44822b56e488a29c1c6500d6e0056bb23e328f87
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-12-07 01:31:06 +0200

    feat: add Montserrat font to the project.

--- lib/view/dash_board_view.dart
+++ lib/view/dash_board_view.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/theme/app_colors.dart';
+import 'package:responsive_dash_board/core/theme/app_text_styles.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: [
+          Text(
+            'yomnaaaa',
+            style: TextStyle(
+              color: AppColors.secondary,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            'yomnaaaa',
+            style: AppTextStyles.styleSemiBold18,
+          ),
+        ],
+      )),
+    );
   }
 }
